781eea3d06a3574f953055dc287fb038d7402ddf
Hugo Finote <34668254+finotty@users.noreply.github.com>
2025-03-13 15:22:52 -0300

    Update BD.js

--- src/firebaseBD/BD.js
+++ src/firebaseBD/BD.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 const firebaseConfig = {
-  apiKey: "AIzaSyDu0fglKn5aINXykQZsg7IIIfxiWxos1SY",
-  authDomain: "encontro-casais.firebaseapp.com",
-  projectId: "encontro-casais",
-  storageBucket: "encontro-casais.appspot.com",
-  messagingSenderId: "53451684606",
-  appId: "1:53451684606:web:4738edf5d0d364974e83af"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
